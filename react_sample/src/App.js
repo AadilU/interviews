c44bc0a26351d32ae60b5663b50dec366b56a482
Divyank Shah <divyank.shah.2016@gmail.com>
2022-09-12 14:58:13 -0700

    add react sample

---
+++ react_sample/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HelloWorld from "./components/HelloWorld";
+
+function App() {
+  return <HelloWorld />;
+}
+
+export default App;
